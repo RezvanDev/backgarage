809446727c2f9f5e64b6f1f02c5af53cf977f7a7
max <max@MacBook-Air-max.local>
2024-08-28 17:27:49 +0500

    Ваше сообщение коммита

--- src/part/part.controller.ts
+++ src/part/part.controller.ts
@@ -1,23 +1,37 @@
-import {Body, Controller, Get, Headers, Post} from '@nestjs/common';
-import {PartService} from "./part.service";
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
+import { PartService } from "./part.service";
 
 @Controller('part')
 export class PartController {
-    constructor(private readonly partService: PartService ) {}
+  constructor(private readonly partService: PartService) {}
 
-    @Get('')
-    async getAll(@Body() body: any) {
-        return this.partService.getAll(body.carId)
-    }
+  @Get('')
+  async getAll(@Body() body: any) {
+    return this.partService.getAll(body.carId)
+  }
 
-    @Get(':id')
-    async getById(@Body() body: any) {
-        return this.partService.getById(body.id)
-    }
+  @Get(':id')
+  async getById(@Body() body: any) {
+    return this.partService.getById(body.id)
+  }
 
-    @Post('')
-    async createPart(@Body() body: any, @Headers('user-id') userId: string) {
-        const parsedUserId = parseInt(userId, 10);  3
-        return this.partService.createPart(body.requestId, body.name, body.new, body.original, body.manufacturer, body.numberOrName, body.price, body.image, parsedUserId, body.carId)
-    }
+  @Post('')
+  async createPart(@Body() body: any, @Headers('user-id') userId: string) {
+    const parsedUserId = parseInt(userId, 10);
+    // Извлекаем URL изображения из объекта, если он есть
+    const imageUrl = body.image && body.image.imageUrl ? body.image.imageUrl : null;
+    
+    return this.partService.createPart(
+      body.requestId,
+      body.name,
+      body.new,
+      body.original,
+      body.manufacturer,
+      body.numberOrName,
+      body.price,
+      imageUrl,  // Теперь передаем строку URL или null
+      parsedUserId,
+      body.carId
+    )
+  }
 }
