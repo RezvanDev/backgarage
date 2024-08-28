243c121d375c2587fa2937751e1160893a5963aa
max <max@MacBook-Air-max.local>
2024-08-28 17:10:01 +0500

    Ваше сообщение коммита

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -5,20 +5,19 @@ import { Response } from 'express';
 
 @Controller('image')
 export class ImageController {
-    constructor(private readonly imageService: ImageService) {}
+  constructor(private readonly imageService: ImageService) {}
 
-    @Post('')
-    @UseInterceptors(FileInterceptor('image'))
-    async uploadImage(@UploadedFile() image: any, @Res() res: Response): Promise<any> {
-        if (!image) {
-            return res.status(400).json({ message: 'Файл не был загружен' });
-        }
-
-        try {
-            const result = await this.imageService.uploadImage(image);
-            return res.status(200).json(result);
-        } catch (error) {
-            return res.status(500).json({ message: 'Ошибка при загрузке файла', error: error.message });
-        }
+  @Post('')
+  @UseInterceptors(FileInterceptor('image'))
+  async uploadImage(@UploadedFile() image?: any, @Res() res: Response): Promise<any> {
+    if (!image) {
+      return res.status(200).json({ message: 'Изображение не было загружено', imageUrl: null });
     }
+    try {
+      const result = await this.imageService.uploadImage(image);
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(500).json({ message: 'Ошибка при загрузке файла', error: error.message });
+    }
+  }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -4,14 +4,15 @@ import * as fs from "fs";
 
 @Injectable()
 export class ImageService {
-    async uploadImage(file: any): Promise<string> {
-        const timestamp = Date.now();
-        const random = Math.floor(Math.random() * 1000);
-        const filename = `${timestamp}-${random}-${file.originalname}`;
-        const destination = path.join(process.cwd(), 'uploads', filename);
-
-        await fs.promises.writeFile(destination, file.buffer);
-
-        return `/uploads/${filename}`;
+  async uploadImage(file?: any): Promise<{ imageUrl: string | null }> {
+    if (!file) {
+      return { imageUrl: null };
     }
+    const timestamp = Date.now();
+    const random = Math.floor(Math.random() * 1000);
+    const filename = `${timestamp}-${random}-${file.originalname}`;
+    const destination = path.join(process.cwd(), 'uploads', filename);
+    await fs.promises.writeFile(destination, file.buffer);
+    return { imageUrl: `/uploads/${filename}` };
+  }
 }
